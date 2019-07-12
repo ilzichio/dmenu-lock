da4349c48de49fecc64523e11bf35b34db957ec1
Comfy Pepe <test1-for-fun@yandex.ru>
2019-07-12 21:07:19 +0300

    Update lock.sh

--- lock.sh
+++ lock.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+hash_file="$HOME/.hash"
+
 lock_action()
 {
         i3-msg 'workspace lock'
@@ -12,7 +14,7 @@ unlock_action()
 
 checkpass()
 {
-        if [ "$(echo "$1" | sha256sum | awk '{print $1;}')" == "$(cat $HOME/.hash)" ]
+        if [ "$(echo "$1" | sha256sum | awk '{print $1;}')" == "$(cat $hash_file)" ]
         then
                 echo true
         fi
@@ -28,5 +30,28 @@ ask()
                 ask
         fi
 }
-lock_action
-ask
+
+newpass()
+{
+        echo -n "Enter new password: "; read np; \
+                        echo "$np" | sha256sum | awk '{print $1;}' > $hash_file
+}
+
+addpass()
+{
+        if [ -f $hash_file ] ; then
+                echo -n "Enter current password: "; read p
+                 if [ "$(checkpass "$p")" == "true" ]; then
+                        newpass
+                 else
+                        echo "Wrong password"; exit 0
+                 fi
+        else
+                newpass
+        fi
+}
+
+case $1 in
+        add) addpass;;
+        *) lock_action; ask;;
+esac
